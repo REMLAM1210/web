32a7bcd31a4e2c2b16900c9cb2ea4bbbc5fe3f1d
REMLAM1210 <2983618236@qq.com>
2026-04-14 13:56:53 +0800

    添加了part1.2·1.3文件

---
+++ part1.2`1.3/src/App.jsx
@@ -0,0 +1,69 @@
+const Header=(props)=>{
+  console.log(props)
+  return(
+    <div>
+      <p>{props.course}</p>
+    </div>
+  )
+}
+const Part=(props)=>{
+  console.log(props)
+  return(
+    <div>
+      <p>{props.name} {props.exercises}</p>
+    </div>
+
+  )
+    
+}
+
+
+const Content=(props)=>{
+  console.log(props)
+  return(
+    <div>
+     <Part name={props.part1} exercises={props.exercises1} />
+      <Part name={props.part2} exercises={props.exercises2} />
+      <Part name={props.part3} exercises={props.exercises3} />
+    </div>
+  )
+}
+
+const Total=(props)=>{
+  console.log(props)
+ return (
+  <div>
+    <p>total number: {props.total}</p>
+  </div>
+ )
+}
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  return (
+    <div>
+      <Header course={course}/>
+      <Content part1={part1.name} exercises1={part1.exercises}/>
+      <Content part2={part2.name} exercises2={part2.exercises}/>
+      <Content part3={part3.name} exercises3={part3.exercises}/>
+      <Total total={part1.exercises+part2.exercises+part3.exercises}/>
+
+
+
+    </div>
+  )
+}
+
+export default App
+   
